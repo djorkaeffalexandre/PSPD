330d1e2765bbd8b8f9866e7219bbaa1ef5518621
Gabriela Guedes <gabrielabguedes@gmail.com>
2021-03-21 16:44:42 -0300

    Adiciona parte de verificação de literais falsos
    Co-authored-by: Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
    Signed-off-by: Gabriela Guedes <gabrielabguedes@gmail.com>

--- src/main.c
+++ src/main.c
@@ -9,6 +9,16 @@ int abs(int num) {
   return num;
 }
 
+int maior_num_vetor(int* array, int tamanho){
+  int idx_maior = 0;
+  for(int i=0; i < tamanho; i++){
+    if(abs(array[i]) >= abs(array[idx_maior])){
+      idx_maior = i;
+    }
+  }
+  return idx_maior;
+}
+
 void verifica_satisfaz(
   int n_clausulas,
   int n_variaveis,
@@ -18,14 +28,22 @@ void verifica_satisfaz(
   int contador = 0;
   int clausula_satisfeita = 0;
   int contador_clausulas_nao_satisfeitas = 0;
+  int contador_variaveis_nao_satisfeitas = 0;
 
   // Index de cláusulas não satisfeitas
   int* clausulas_nao_satisfeitas = (int*) calloc(n_clausulas, sizeof(int));
+  int* variaveis_nao_satisfeitas = (int*) calloc(n_variaveis, sizeof(int));
 
   for (int i = 0; i < n_clausulas; i++) {
+    int* variaveis_nao_satisfeitas_teste = (int*) calloc(n_variaveis, sizeof(int));
     for (int j = 0; j < n_variaveis; j++) {
       int variavel = clausulas[i][j];
       int valor;
+
+      // Se a clausula não tiver essa variavel pula para a proxima
+      if(variavel == 0){
+        continue;
+      }
 
       // Se a variável não for uma negação
       // Terá o valor igual ao valor lido
@@ -40,6 +58,8 @@ void verifica_satisfaz(
       // Se o valor é verdadeiro
       if (valor > 0) {
         clausula_satisfeita = 1;
+      } else {
+        variaveis_nao_satisfeitas_teste[j]++;
       }
     }
     if (clausula_satisfeita == 1) {
@@ -48,6 +68,13 @@ void verifica_satisfaz(
     } else {
       clausulas_nao_satisfeitas[i] = 1;
       contador_clausulas_nao_satisfeitas++;
+      for(int k=0; k < n_variaveis; k++){
+        int antes = variaveis_nao_satisfeitas[k];
+        variaveis_nao_satisfeitas[k] += variaveis_nao_satisfeitas_teste[k];
+        if(antes == 0 && variaveis_nao_satisfeitas[k] > 0){
+          contador_variaveis_nao_satisfeitas++;
+        }
+      }
     }
   }
   // A cláusula foi satisfeita
@@ -56,7 +83,7 @@ void verifica_satisfaz(
   // A cláusula não foi satisfeita
   } else {
     // Imprime os índexes das cláusulas que não foram satisfeitas
-    printf("[%d clausulas nao satisfeitas] ", contador_clausulas_nao_satisfeitas);
+    printf("[%d clausulas falsas] ", contador_clausulas_nao_satisfeitas);
     for (int i = 0; i < n_clausulas; i++) {
       if (clausulas_nao_satisfeitas[i] == 1) {
         printf("%d", i);
@@ -67,8 +94,18 @@ void verifica_satisfaz(
       }
     }
     printf("\n");
-    // Imprime os literais falsos
-    // TODO
+    // Print das variaveis
+    printf("[lits] ");
+    while(contador_variaveis_nao_satisfeitas > 0){
+      int idx = maior_num_vetor(variaveis_nao_satisfeitas, n_variaveis);
+      printf("%d", valores[idx] * -1);
+      variaveis_nao_satisfeitas[idx] = 0;
+      contador_variaveis_nao_satisfeitas--;
+      if (contador_variaveis_nao_satisfeitas > 0) {
+        printf(" ");
+      }
+    }
+    printf("\n");
   }
 }
 
@@ -77,10 +114,10 @@ int main() {
   scanf("%d %d", &n_variaveis, &n_clausulas);
 
   // int[clausula][variaveis]
-  int **clausulas = (int **) malloc(n_clausulas * sizeof(int*));
+  int **clausulas = (int **) calloc(n_clausulas, sizeof(int*));
 
   for(int i = 0; i < n_clausulas; i++) {
-    clausulas[i] = (int *) malloc(n_variaveis * sizeof(int));
+    clausulas[i] = (int *) calloc(n_variaveis, sizeof(int));
 
     int num;
     do {
@@ -92,7 +129,7 @@ int main() {
   }
 
   char comando[4];
-  int* valores = (int *) malloc(n_variaveis * sizeof(int));
+  int* valores = (int *) calloc(n_variaveis, sizeof(int));
   while(scanf("%4s", comando) != EOF) {
     if (strcmp(comando, "full") == 0) {
       for (int i = 0; i < n_variaveis; i++) {
