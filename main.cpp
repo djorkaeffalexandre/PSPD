996bca648d6bd8c7ca65e042333b08d97eb24705
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2021-04-28 22:11:43 -0300

    Alterações para pular cláusulas já calculadas

--- main.cpp
+++ main.cpp
@@ -4,162 +4,216 @@
 #include <string>
 #include <cstring>
 #include <algorithm>
+#include <unordered_set>
+#include <set>
+#include <iterator>
+
+#define LITS true
+#define PRINT true
 
 // g++ -std=c++17 -O2 main.cpp -o prog
 
-// Desativa todos os LOGS
-#define DEBUG true
-// Desativa a lista de variáveis
-#define LISTS true
-
 using namespace std;
 
-void check(
-  int n_variaveis,
-  int n_clausulas,
-  vector<vector<int> > clausulas,
-  vector<int> valores
-) {
-  vector<int> idx_clausulas_nao_satisfeitas;
-  vector<int> valores_negativos(n_variaveis, 0);
-  int n_valores_negativos = 0;
+int n_variaveis, n_clausulas;
+vector<vector<int> > clausulas;
+vector<vector<bool> > l_clausulas;
+unordered_set<int> idx_clausulas_nao_satisfeitas;
+#if LITS
+vector<int> literais_falsos;
+#endif
+vector<bool> valores;
 
-  bool flag_expressao = true;
-  for (int i = 0; i < clausulas.size(); i++) {
-    bool flag_clausula = false;
+void le_entrada();
+void le_comandos();
+void executa_full();
+void executa_flip();
+void imprime_resultado();
 
-    for (int j = 0; j < clausulas[i].size(); j++) {
-      int base = clausulas[i][j];
-      
-      int value = valores[abs(base) - 1];
-      if (base < 0) {
-        value *= -1;
-      }
-
-      if (value > 0) {
-        // Se encontramos um valor positivo a cláusula é verdadeira
-        // e portanto os valores negativos não importam mais
-        // então podemos ir para a próxima cláusula
-        flag_clausula = true;
-        break;
-      }
-    }
-
-    if (flag_clausula) {
-      // Do nothing
-    } else {
-      flag_expressao = false;
-      idx_clausulas_nao_satisfeitas.push_back(i);
-      #if LISTS
-      for (int j = 0; j < clausulas[i].size(); j++) {
-        int base = clausulas[i][j];
-        int value = valores[abs(base) - 1];
-        if (base < 0) {
-          value *= 1;
-        }
-        int literal = abs(base);
-        if (valores_negativos[literal - 1] == 0) {
-          n_valores_negativos += 1;
-        }
-        valores_negativos[literal - 1] += 1;
-      }
-      #endif
-    }
+bool compara(pair<int, int>& a, pair<int, int>& b) {
+  if (a.second == b.second) {
+    return abs(a.first) > abs(b.first);
   }
-
-  if (flag_expressao) {
-    #if DEBUG
-    printf("SAT\n");
-    #endif
-  } else {
-    int n_clausulas_nao_satisfeitas = idx_clausulas_nao_satisfeitas.size();
-    #if DEBUG
-    printf("[%d clausulas falsas] ", n_clausulas_nao_satisfeitas);
-    #endif
-    for (int i = 0; i < n_clausulas_nao_satisfeitas; i++) {
-      #if DEBUG
-      printf("%d", idx_clausulas_nao_satisfeitas[i]);
-      #endif
-      if (i != n_clausulas_nao_satisfeitas - 1) {
-        #if DEBUG
-        printf(" ");
-        #endif
-      }
-    }
-    #if DEBUG
-    printf("\n");
-    #endif
-
-    #if LISTS
-    #if DEBUG
-    printf("[lits] ");
-    #endif
-    while (n_valores_negativos > 0) {
-      int idx = distance(
-        valores_negativos.begin(),
-        max_element(
-          valores_negativos.begin(),
-          valores_negativos.end(),
-          [](const int& a, const int& b) {
-            return abs(a) <= abs(b);
-          }
-        )
-      );
-      printf("%d", valores[idx] * -1);
-      valores_negativos[idx] = 0;
-      n_valores_negativos--;
-      if (n_valores_negativos > 0) {
-        #if DEBUG
-        printf(" ");
-        #endif
-      }
-    }
-    #if DEBUG
-    printf("\n");
-    #endif
-    #endif
-  }
+  return a.second > b.second;
 }
 
 int main() {
-  int n_variaveis, n_clausulas;
   
+  le_entrada();
+  le_comandos();
+
+  return 0;
+}
+
+void le_entrada() {
   scanf("%d", &n_variaveis);
   scanf("%d", &n_clausulas);
 
-  vector<vector<int> > clausulas;
-  vector<int> variaveis;
+  l_clausulas.resize(n_variaveis, vector<bool>(n_clausulas));
+  valores.resize(n_variaveis, false);
+  #if LITS
+  literais_falsos.resize(n_variaveis, 0);
+  #endif
 
   for (int i = 0; i < n_clausulas; i++) {
+    vector<int> literais;
     int num;
     do {
       scanf("%d", &num);
       if (num != 0) {
-        variaveis.push_back(num);
+        literais.push_back(num);
+        l_clausulas[abs(num) - 1][i] = true;
       }
     } while (num != 0);
-    clausulas.push_back(variaveis);
-    variaveis.clear();
+    clausulas.push_back(literais);
+    literais.clear();
   }
+}
 
+void le_comandos() {
   char comando[4];
-  vector<int> valores(n_variaveis);
   while(scanf("%4s", comando) != EOF) {
     if (strcmp(comando, "full") == 0) {
-      for (int i = 0; i < n_variaveis; i++) {
-        int variavel;
-        scanf("%d", &variavel);
-        valores[i] = variavel;
-      }
-      check(n_variaveis, n_clausulas, clausulas, valores);
+      executa_full();
     }
     if (strcmp(comando, "flip") == 0) {
-      int variavel;
-      scanf("%d", &variavel);
-      valores[abs(variavel) - 1] = valores[abs(variavel) - 1] * -1;
-      check(n_variaveis, n_clausulas, clausulas, valores);
+      executa_flip();
     }
+    imprime_resultado();
+  }
+}
+
+void executa_full() {
+  idx_clausulas_nao_satisfeitas.clear();
+  #if LITS
+  literais_falsos.clear();
+  #endif
+  int valor;
+  for (int i = 0; i < n_variaveis; i++) {
+    scanf("%d", &valor);
+    valores[abs(valor) - 1] = valor > 0 ? true : false;
   }
 
-  return 0;
+  for (int i = 0; i < n_clausulas; i++) {
+    bool satisfaz = false;
+    for (int j = 0; j < clausulas[i].size(); j++) {
+      int base = clausulas[i][j];
+      int valor = valores[abs(base) - 1] ? base : base * -1;
+      
+      if (valor > 0) {
+        satisfaz = true;
+        break;
+      }
+    }
+    if (!satisfaz) {
+      idx_clausulas_nao_satisfeitas.insert(i);
+      #if LITS
+      for (int j = 0; j < clausulas[i].size(); j++) {
+        int base = clausulas[i][j];
+        int valor = valores[abs(base) - 1] ? base : base * -1;
+        if (valor < 0) {
+          literais_falsos[abs(base) - 1] += 1;
+        }
+      }
+      #endif
+    }
+  }
+}
+
+void executa_flip() {
+  int valor;
+  scanf("%d", &valor);
+  valores[abs(valor) - 1] = !valores[abs(valor) - 1];
+
+  for (int i = 0; i < n_clausulas; i++) {
+    if (!l_clausulas[abs(valor) - 1][i]) {
+      continue;
+    }
+    bool satisfaz = false;
+    for (int j = 0; j < clausulas[i].size(); j++) {
+      int base = clausulas[i][j];
+
+      int valor = valores[abs(base) - 1] ? base : base * -1;
+      
+      if (valor > 0) {
+        satisfaz = true;
+        break;
+      }
+    }
+    if (satisfaz) {
+      if (idx_clausulas_nao_satisfeitas.find(i) != idx_clausulas_nao_satisfeitas.end()) {
+        #if LITS
+        for (int j = 0; j < clausulas[i].size(); j++) {
+          int base = clausulas[i][j];
+          bool flip = valores[abs(base) - 1];
+          if (abs(base) == valor) {
+            flip = !flip;
+          }
+          int valor = flip ? base : base * -1;
+          if (valor < 0) {
+            literais_falsos[abs(base) - 1] -= 1;
+          }
+        }
+        #endif
+        idx_clausulas_nao_satisfeitas.erase(i);
+      }
+    } else {
+      if (idx_clausulas_nao_satisfeitas.find(i) == idx_clausulas_nao_satisfeitas.end()) {
+        idx_clausulas_nao_satisfeitas.insert(i);
+        #if LITS
+        for (int j = 0; j < clausulas[i].size(); j++) {
+          int base = clausulas[i][j];
+          int valor = valores[abs(base) - 1] ? base : base * -1;
+          if (valor < 0) {
+            literais_falsos[abs(base) - 1] += 1;
+          }
+        }
+        #endif
+      }
+    }
+  }
+}
+
+void imprime_resultado() {
+  if (idx_clausulas_nao_satisfeitas.empty()) {
+    #if PRINT
+    printf("SAT\n");
+    #endif
+  } else {
+    int size = idx_clausulas_nao_satisfeitas.size();
+    #if PRINT
+    printf("[%d clausulas falsas]", size);
+    #endif
+    set<int> ordered(idx_clausulas_nao_satisfeitas.begin(), idx_clausulas_nao_satisfeitas.end());
+    for (const auto& element: ordered) {
+      #if PRINT
+      printf(" %d", element);
+      #endif
+    }
+    #if PRINT
+    printf("\n");
+    #endif
+    #if LITS
+    #if PRINT
+    printf("[lits]");
+    #endif
+    vector<pair<int, int>> lits;
+    for (int i = 0; i < n_variaveis; i++) {
+      if (literais_falsos[i] > 0) {
+        int literal = (i + 1) * (valores[i] ? 1 : -1) * -1;
+        int contador = literais_falsos[i];
+        lits.push_back(make_pair(literal, contador));
+      }
+    }
+    sort(lits.begin(), lits.end(), compara);
+    for (int i = 0; i < lits.size(); i++) {
+      #if PRINT
+      printf(" %d", lits[i].first);
+      #endif
+    }
+    #if PRINT
+    printf("\n");
+    #endif
+    #endif
+  }
 }
